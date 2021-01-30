bb67e7b2eb80131495a5c8c9e4e2fb0c9cf11fec
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2021-01-29 19:11:14 -0500

    Connect mongodb to backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_DATABASE_URI)
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 
@@ -19,29 +31,30 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully!',
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: 'Post added successfully!',
+      postId: createdPost._id,
+    });
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '1',
-      title: 'First Post',
-      content: 'This is from the server.',
-    },
-    {
-      id: '2',
-      title: 'Second Post',
-      content: 'This is from the server!',
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts,
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: 'Posts fetched successfully!',
+      posts: documents,
+    });
+  });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then(() => {
+    res.status(200).json({ message: 'Post deleted!' });
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -13,12 +14,20 @@ export class PostsService {
 
   getPosts() {
     this.httpClient
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              id: post._id,
+              title: post.title,
+              content: post.content,
+            };
+          });
+        })
       )
-      .subscribe((postData) => {
-        console.log(postData.message);
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -30,10 +39,23 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.httpClient
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string; postId: string }>(
+        'http://localhost:3000/api/posts',
+        post
+      )
       .subscribe((resData) => {
-        console.log(resData.message);
+        const id = resData.postId;
+        post.id = id;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.httpClient
+      .delete<{ message: string }>('http://localhost:3000/api/posts/' + postId)
+      .subscribe((resData) => {
+        this.posts = this.posts.filter((post) => post.id !== postId);
         this.postsUpdated.next([...this.posts]);
       });
   }
